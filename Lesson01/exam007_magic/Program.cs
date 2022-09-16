1ce1f5102d5fc3aaf4f304d04f4d154f8fa5c51e
Nikita <nikitafox770@gmail.com>
2022-09-16 21:28:40 +0500

    les01

--- Lesson01/exam007_magic/Program.cs
+++ Lesson01/exam007_magic/Program.cs
@@ -19,26 +19,26 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 10000)
+while (count < 10000)
 {
     int what = new Random().Next(0, 3); // [0,3) 0 1 2
-    if(what == 0)
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
 
-    if(what == 1)
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
 
-    if(what == 2)
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
-    } 
+    }
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
